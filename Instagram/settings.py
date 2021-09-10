d7a2b469fa84bbf03eafc06b744e01450d3b34a4
Usama <usamakaleem505@gmail.com>
2021-09-10 22:27:45 +0500

    udpated

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.linkedin.LinkedinOAuth2',
